539f1122698de3ab241e4bcc3cefa8bb0ceabc73
Tiag0dd <tiagogeremiasmagne@gmail.com>
2026-05-06 23:26:58 -0300

    Primer versión de mi portfolio, con funciones básicas y css específico.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { About } from './pages/about/about';
+import { Navbar } from './layout/navbar/navbar';
+import { Footer } from './layout/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'projects', component: Projects },
+  { path: 'about', component: About },
+  {path: 'navbar', component: Navbar },
+  {path: 'footer', component: Footer},
+];
