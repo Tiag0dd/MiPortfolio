7712a9ffb1d13cd91b0fa77734b327629a0909aa
Tiag0dd <tiagogeremiasmagne@gmail.com>
2026-06-19 00:18:40 -0300

    Implementé servicios para autocompletar los proyectos.

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Data } from '../../services/projectService';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  standalone: true, 
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {}
+export class Projects {
+  proyectosGithub: any[] = [];
+  otrasExperiencias: any[] = [];
+
+  constructor(private projectGit: Data) {
+    this.projectGit.getProjectsGit().subscribe({
+      next: (data: any) => {
+        this.proyectosGithub = data;
+      },
+      error: (error) => console.error(error),
+    });
+
+    this.projectGit.getProjectsList().subscribe({
+      next: (data: any) => {
+        this.otrasExperiencias = data;
+      },
+      error: (error) => console.error(error),
+    });
+  }
+}
